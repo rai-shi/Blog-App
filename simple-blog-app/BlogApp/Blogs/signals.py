e043792e756962b7e5b66d5708277cfcfb7ebb19
Aysenur <aysenurtak1@gmail.com>
2025-02-27 19:22:18 +0300

    search: blog index indexing is done

---
+++ simple-blog-app/BlogApp/Blogs/signals.py
@@ -0,0 +1,55 @@
+from django.db.models.signals import post_save, post_delete, m2m_changed
+from django.dispatch import receiver
+
+from django_elasticsearch_dsl.registries import registry
+
+from .models import * 
+from Users.models import User
+from Search.indexes import UserIndex, BlogIndex
+
+import datetime
+
+# blog creation signals
+@receiver(post_save, sender=Blog)
+def create_blog_index(sender, instance, created, **kwargs):
+    print(f"Blog {instance.title} created: {created}")
+    if created:
+        elasticsearch_blog(instance)
+        print(f"Blog index created for {instance.title}")
+
+@receiver(m2m_changed, sender=Blog.categories.through)
+def update_blog_index_on_category_change(sender, instance, action, **kwargs):
+    if action in ["post_add"]: 
+        print(f"Categories updated for {instance.title}")
+        elasticsearch_blog(instance)
+
+def elasticsearch_blog(instance):
+    categories = list(instance.categories.all())
+    # comments = list(instance.comments.all())
+    print(f"Categories: {categories}") 
+    # print(f"Comments: {comments}")
+    print(type(instance.created_at))
+    blog_doc = BlogIndex(
+        meta={"id": instance.id},
+        title=instance.title,
+        description=instance.description,
+        content=instance.content,
+        created_at=instance.created_at,
+        author_username=instance.author.username,
+        author_id=instance.author.id,
+
+        categories=[{"id": cat.id, "name": cat.name} for cat in categories],
+        
+        like_count=0,
+        comment_count=0
+        )
+    blog_doc.save()
+
+
+
+
+
+# blog deletion signals
+# blog update signals
+# blog comment signals
+# blog like signals
--- simple-blog-app/BlogApp/Users/signals.py
+++ simple-blog-app/BlogApp/Users/signals.py
@@ -1,48 +1,22 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, m2m_changed
 from django.dispatch import receiver
 from .models import User, Profile
 from Search.indexes import UserIndex
 
 
-# @receiver(post_save, sender=Profile)
-# def update_user_index_on_profile_save(sender, instance, **kwargs):
-#     user = instance.user  
-#     print(f"Profile updated for {user.username}")
-#     print(instance.user.profile.categories) # Blogs.Category.None
-#     user_doc = UserIndex(
-#         meta={"id": user.id},
-#         first_name=user.first_name,
-#         last_name=user.last_name,
-#         full_name=f"{user.first_name} {user.last_name}",
-#         username=user.username,
-#         profile={
-#             "bio": instance.bio,
-#             "categories": [{
-#                 "id": category.id, 
-#                 "name": category.name} for category in instance.categories.all()]
-#         }
-#     )
-#     user_doc.save()
-#     print(f"Elasticsearch index updated for {user.username}")
-
-
-from django.db.models.signals import post_save, m2m_changed
-from django.dispatch import receiver
-from .models import Profile
-from Search.indexes import UserIndex
-
 @receiver(post_save, sender=Profile)
-def update_user_index_on_profile_save(sender, instance, **kwargs):
-    print(f"Profile updated for {instance.user.username}")
-    update_elasticsearch(instance)
+def create_user_index_on_profile_save(sender, instance, **kwargs):
+    print(f"Profile created for {instance.user.username}")
+    elasticsearch_user(instance)
 
 @receiver(m2m_changed, sender=Profile.categories.through)
 def update_user_index_on_category_change(sender, instance, action, **kwargs):
-    if action in ["post_add", "post_remove"]:
+    if action in ["post_add"]: # ??
         print(f"Categories updated for {instance.user.username}")
-        update_elasticsearch(instance)
+        elasticsearch_user(instance)
 
-def update_elasticsearch(instance):
+
+def elasticsearch_user(instance):
     user = instance.user
 
     categories = list(instance.categories.all())
